ec33a41168db174c884ea682cbe3c0bf6d6830b1
Victor Hugo Camargo <vcamargo.e@gmail.com>
2024-03-23 08:26:19 -0500

    WIP : Comm

--- api.py
+++ api.py
@@ -1,4 +1,6 @@
 from fastapi import FastAPI
+from datetime import datetime
+from pydantic import BaseModel
 
 # Create an instance of the FastAPI class
 app = FastAPI()
@@ -9,7 +11,20 @@ async def read_root():
     return {"message": "Hello, World!"}
 
 @app.get("/signal-read")
-async def read_singa():
+async def read_signal():
     with open("output.txt", "r") as file:
-        line = file.readlines(1)
+        line = file.readlines(-1)
         return {"line": line}
+    
+
+class SingalCapture(BaseModel):
+    value: float
+    source: str
+    timestamp: str = None
+
+@app.post("/signal-capture")
+async def create_signal(input: SingalCapture):
+    input.timestamp = input.timestamp or str(datetime.now())
+    with open("output.txt", "a") as file:
+        file.write(f"{input.model_dump()}\n")
+    return {"message": "Signal captured successfully"}
--- app.py
+++ app.py
@@ -6,26 +6,40 @@ from time import sleep
 from random import random
 import json
 
-def main():
-    processor = AggregatorProcessor()
-    for i in range(10):
-        input_read = InputRead(
-            value=random() * 100,
-            timestamp=datetime.now(),
-            source="source"
-        )
+from serial import Serial
 
-        with open("output.txt", "a") as file:
-            result = processor.process(input_read)
-            output = {
-                "timestamp": str(input_read.timestamp),
-                "value": input_read.value,
-                "source": input_read.source,
-                "mean": result.mean_value,
-                "change_rate": result.change_rate,
-            }
-            file.write(json.dumps(output) + "\n")
-            print(output)
+port = Serial("/dev/ttyACM0", 115200)
+
+class Comm:
+    def __init__(self, port: Serial, header: int):
+        self._port = port
+        self._header = header
+        self._input_buffer = []
+
+    def has_message(self) -> bool:
+        if self._port.in_waiting > 0:
+            input_header = ord(self._port.read())
+            if self._header == input_header:
+                length = ord(self._port.read())
+                data = [byte for byte in self._port.read(size=length)]
+                checksum = ord(self._port.read())
+                calculated_checksum = ~sum(data) & 0xFF
+                if checksum != calculated_checksum:
+                    print("Checksum error")
+                    return False
+                self._input_buffer += data
+                return True
+        return False
+    
+    def get_queue(self) -> list[int]:
+        return self._input_buffer
+
+comm = Comm(port, 0x7e)
+
+def main():
+    for i in range(20):
+        if comm.has_message():
+            print(f"Message: {comm.get_queue()}")
         sleep(1)
 
 
